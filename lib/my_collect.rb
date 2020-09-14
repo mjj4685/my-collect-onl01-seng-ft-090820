120823ec1b1edeb5b2b94ba88b7b72bf315d88bc
MJ Johnson <mjohnson4685@gmail.com>
2020-09-14 00:34:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+collection = ["Max", "Mia", "Bella"]
 
 
+def my_collect(array)
+  i = 0
+  names = []
+  while i < array.length
+    names.push yield(array[i])
+    i += 1
+  end
+  names
+end
+
+my_collect(collection) {|i| i.split(" ").first}
